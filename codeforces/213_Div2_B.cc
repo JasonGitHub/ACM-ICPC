c2cd4bf98ec11a1e8444100d3c7ba3f003f576bc
jason <jasonwu2011@gmail.com>
2013-11-20 23:44:40 +0800

    update

--- codeforces/213_Div2_B.cc
+++ codeforces/213_Div2_B.cc
@@ -9,12 +9,12 @@ inline int len(int l, int r) {
 int FindLongestSeg(int* a, int n) {
   if (n <= 2) return n;
   bool in_seg = false;
-  int max = 0, l = 2;
-  for (int i = 2; i < n; ++i) {
-    if (!in_seg) l = i;
-    if (in_seg && a[i] != a[i - 1] + a[i - 2]) in_seg = false;
-    if (!in_seg && a[i] == a[i - 1] + a[i - 2]) in_seg = true;
-    if (in_seg) max = (len(l, i) > max) ? len(l, i) : max;
+  int max = 0;
+  for (int l = 2, r = 2; r < n; ++r) {
+    if (!in_seg) l = r;
+    if (in_seg && a[r] != a[r - 1] + a[r - 2]) in_seg = false;
+    if (!in_seg && a[r] == a[r - 1] + a[r - 2]) in_seg = true;
+    if (in_seg) max = (len(l, r) > max) ? len(l, r) : max;
   }
   return max + 2;
 }
@@ -23,9 +23,7 @@ int main() {
   int n;
   int a[100000 + 10];
   cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> a[i];
-  }
+  for (int i = 0; i < n; ++i) cin >> a[i];
   cout << FindLongestSeg(a, n) << endl;
   return 0;
 }
