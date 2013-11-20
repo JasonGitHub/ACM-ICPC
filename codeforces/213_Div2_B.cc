b31538efb5fd7b672b15ef17a3969da4c4e3b184
jason <jasonwu2011@gmail.com>
2013-11-20 23:38:44 +0800

    update

--- codeforces/213_Div2_B.cc
+++ codeforces/213_Div2_B.cc
@@ -2,23 +2,20 @@
 
 using namespace std;
 
+inline int len(int l, int r) {
+  return r - l + 1;
+}
+
 int FindLongestSeg(int* a, int n) {
   if (n <= 2) return n;
   bool in_seg = false;
-  int max = 0, lo = 2, hi = 2;
+  int max = 0, l = 2;
   for (int i = 2; i < n; ++i) {
-    if (in_seg) {
-      if (a[i] != a[i - 1] + a[i - 2]) {
-        in_seg = false;
-        max = (hi - lo + 1 > max) ? hi - lo + 1 : max;
-      }
-    } else {
-      if (a[i] == a[i - 1] + a[i - 2]) in_seg = true;
-      lo = i;
-    }
-    hi = i;
+    if (!in_seg) l = i;
+    if (in_seg && a[i] != a[i - 1] + a[i - 2]) in_seg = false;
+    if (!in_seg && a[i] == a[i - 1] + a[i - 2]) in_seg = true;
+    if (in_seg) max = (len(l, i) > max) ? len(l, i) : max;
   }
-  if (in_seg) max = (hi - lo + 1 > max) ? hi - lo + 1 : max;
   return max + 2;
 }
 
