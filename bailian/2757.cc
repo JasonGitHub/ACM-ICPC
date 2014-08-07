13b1e5a43b2245dae21139a66868e6c8700b70fb
jason <jasonwu2011@gmail.com>
2014-08-07 17:15:27 +0800

    2757 我为人人

--- bailian/2757.cc
+++ bailian/2757.cc
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// 人人为我
 int max_len_increasing_subsequence(const vector<int> &a) {
   const int n = a.size();
   vector<int> max_len(n, 1);
@@ -11,6 +12,20 @@ int max_len_increasing_subsequence(const vector<int> &a) {
     for (int j = 0; j < i; ++j) {
       if (a[j] < a[i]) {
         max_len[i] = max(max_len[i], max_len[j] + 1);
+      }
+    }
+  }
+  return *max_element(max_len.begin(), max_len.end());
+}
+
+// 我为人人
+int max_len_increasing_subsequence_1(const vector<int> &a) {
+  const int n = a.size();
+  vector<int> max_len(n, 1);
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      if (a[j] > a[i]) {
+        max_len[j] = max(max_len[j], max_len[i] + 1);
       }
     }
   }
