577789c7b6b9108b825500b0ee189583d3c43e4b
jason <jasonwu2011@gmail.com>
2014-08-07 15:03:58 +0800

    fixed 2757

--- bailian/2757.cc
+++ bailian/2757.cc
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int max_len_increasing_subsequence(const vector<int> &a) {
   const int n = a.size();
   vector<int> max_len(n, 1);
-  for (int i = 0; i < n; ++i) {
+  for (int i = 1; i < n; ++i) {
     for (int j = 0; j < i; ++j) {
       if (a[j] < a[i]) {
         max_len[i] = max(max_len[i], max_len[j] + 1);
       }
     }
   }
-  return max_len[n - 1];
+  return *max_element(max_len.begin(), max_len.end());
 }
 
 int main() {
